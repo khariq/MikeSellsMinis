47fe873a64753f8f32f7bb3e96b4aa7a240fa563
Michael Pickens <khariq@gmail.com>
2015-04-28 08:52:55 -0400

    initial

---
+++ scripts/app/app.js
@@ -0,0 +1,9 @@
+var app = angular.module("mikeSellsMinis", []);
+
+app.controller('mikeSellsMinis_MainCtrl', ['$scope', function($scope){
+
+}]);
+
+app.factory('mikeSellsMinis_OfferFactory', [function(){
+
+}]);
